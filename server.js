7da9b715af38709842ac63cef8b17e912ea00c94
Pranjali Srivastav <pranjalisrivastav9@gmail.com>
2021-09-14 18:08:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">Welcome to my Node.js project which is deployed in ecs..!</h1> \n');
+  res.send('<html><body><h1 style="color:green">Welcome to my Node.js project which is deployed in ecs..!</h1></body></html> \n');
 });
 
 app.listen(PORT, HOST);
