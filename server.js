67af7e04bb5cb9436b9c6c64883ced1a396c2c6a
raviteja312 <79452564+raviteja312@users.noreply.github.com>
2021-09-14 22:10:01 +0530

    test commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<html><body><h1 style="color:green">Welcome to my Node.js project which is deployed in ecs..!</h1></body></html> \n');
+  res.send('<html><body><h1 style="color:green">Welcome to my Node.js project which is deployed in ecs through ci/cd..!</h1></body></html> \n');
 });
 
 app.listen(PORT, HOST);
